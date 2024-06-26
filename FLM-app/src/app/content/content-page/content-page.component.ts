97322f35618023184f9a96a060eb7b4e1204d935
harrymartensC4 <harry@c4digital.com.au>
2024-06-26 18:23:18 +1000

    Updating Angular UI

--- FLM-app/src/app/content/content-page/content-page.component.ts
+++ FLM-app/src/app/content/content-page/content-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import { ChartData, ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import Chart from 'chart.js/auto';
 
 
 @Component({
@@ -7,13 +10,47 @@ import {FormControl, FormGroup} from '@angular/forms';
   templateUrl: './content-page.component.html',
   styleUrl: './content-page.component.scss'
 })
-export class ContentPageComponent {
-  // userForm = new FormGroup();
-  query = new FormControl('');
 
+export class ContentPageComponent {
+  public chart: any;
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  query = new FormControl('');
 
   print() {
     console.log(this.query.value)
+  }
+
+  createChart(){
+    this.chart = new Chart("MyChart", {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['Cell 1', 'Cell 2', 'Cell 3','Cell 4',
+								 'Cell 5', 'Cell 6', 'Cell 7','Cell 8', ], 
+	       datasets: [
+          {
+            label: "Gene 1",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Gene 2",
+            data: ['542', '542', '536', '327', '17',
+									 '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
   }
 }
 
