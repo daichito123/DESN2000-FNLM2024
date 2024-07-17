097e5cd919f575e51427e976b17775b1397c779d
harrymartensC4 <harry@c4digital.com.au>
2024-07-17 11:45:55 +1000

    UI Implementation

--- FLM-app/src/app/content/content-page/content-page.component.ts
+++ FLM-app/src/app/content/content-page/content-page.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
-import { ChartData, ChartOptions } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
-import Chart from 'chart.js/auto';
+import { QueryServiceService } from '../../query-service.service';
+
 
 
 @Component({
@@ -12,45 +10,16 @@ import Chart from 'chart.js/auto';
 })
 
 export class ContentPageComponent {
-  public chart: any;
-
-  ngOnInit(): void {
-    this.createChart();
+  isExpanded = false;
+  public queryService: QueryServiceService;
+  constructor(queryService:QueryServiceService){
+    this.queryService = queryService;
+  }
+  
+  toggleWidth() {
+    this.isExpanded = !this.isExpanded;
   }
 
-  query = new FormControl('');
 
-  print() {
-    console.log(this.query.value)
-  }
-
-  createChart(){
-    this.chart = new Chart("MyChart", {
-      type: 'bar', //this denotes tha type of chart
-
-      data: {// values on X-Axis
-        labels: ['Cell 1', 'Cell 2', 'Cell 3','Cell 4',
-								 'Cell 5', 'Cell 6', 'Cell 7','Cell 8', ], 
-	       datasets: [
-          {
-            label: "Gene 1",
-            data: ['467','576', '572', '79', '92',
-								 '574', '573', '576'],
-            backgroundColor: 'blue'
-          },
-          {
-            label: "Gene 2",
-            data: ['542', '542', '536', '327', '17',
-									 '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }  
-        ]
-      },
-      options: {
-        aspectRatio:2.5
-      }
-      
-    });
-  }
 }
 
