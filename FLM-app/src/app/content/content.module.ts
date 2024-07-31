c212a39d1740855e3856d48d2a1ba00c60850702
harrymartensC4 <harry@c4digital.com.au>
2024-07-31 19:28:18 +1000

    Frontend Server

--- FLM-app/src/app/content/content.module.ts
+++ FLM-app/src/app/content/content.module.ts
@@ -7,6 +7,7 @@ import {Component} from '@angular/core';
 import { ContentPageComponent } from './content-page/content-page.component';
 import { IntroStateComponent } from './intro-state/intro-state.component';
 import { QueryStateComponent } from './query-state/query-state.component';
+import { DialogContentExampleDialog } from './content-page/content-page.component';
 
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
@@ -14,12 +15,11 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
-
-
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @NgModule({
-  declarations: [ContentPageComponent, IntroStateComponent, QueryStateComponent],
+  declarations: [ContentPageComponent, IntroStateComponent, QueryStateComponent, DialogContentExampleDialog],
   imports: [
     CommonModule,
     MatSidenavModule,
@@ -28,7 +28,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDialogModule
   ],
   exports: [
     ContentPageComponent,
